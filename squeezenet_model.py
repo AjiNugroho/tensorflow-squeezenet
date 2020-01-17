723bfe0a225d0710604833a0b7b8f4688fdc76b4
aji.nugroho <aji.nugroho@sebangsa.com>
2020-01-17 11:11:05 +0700

    update variable tensor

--- squeezenet_model.py
+++ squeezenet_model.py
@@ -48,39 +48,9 @@ class SqueezeNet:
         self.out_classes = out_classes
         self.inputs = tf.placeholder(tf.float32,shape=(None,input_shape[0],input_shape[1],input_shape[2]))
         self.labels = tf.placeholder(tf.float32,shape=(None,self.out_classes))
-        self.loss_v0,self.loss_v0_res,self.loss_v1 = self.model_loss(self.inputs,self.labels,train)     
-        self.v0_opt,self.v0_res_opt,self.v1_opt = self.model_opti(self.loss_v0,self.loss_v0_res,self.loss_v1,self.lr_rate)
+        self.loss_v1 = self.model_loss(self.inputs,self.labels,train)     
+        self.v1_opt = self.model_opti(self.loss_v1,self.lr_rate)
         
-    #Model Definition of SqueezeNet V0
-    def model_arc_v0(self,inputs,train,reuse=False):
-        
-        with tf.variable_scope("squeezenet_v0",reuse=reuse):
-            conv1 = general_conv(inputs,filters=96,kernel=7,stride=2,padding="SAME",name="conv1",relu=True,weight="Xavier")
-            pool1 = tf.layers.max_pooling2d(conv1,pool_size=3,strides=2,name="pool1")
-            
-            fire2 = fire_module(pool1,16,64,64,name="fire2")
-            fire3 = fire_module(fire2,16,64,64,name="fire3")
-            fire4 = fire_module(fire3,32,128,128,name="fire4")
-        
-            pool2 = tf.layers.max_pooling2d(fire4,pool_size=3,strides=2,name="pool2")
-            
-            fire5 = fire_module(pool2,32,128,128,name="fire5")
-            fire6 = fire_module(fire5,48,192,192,name="fire6")
-            fire7 = fire_module(fire6,48,192,192,name="fire7")
-            fire8 = fire_module(fire7,64,256,256,name="fire8")
-        
-            pool3 = tf.layers.max_pooling2d(fire8,pool_size=3,strides=2,name="pool3")
-        
-            fire9 = fire_module(pool3,64,256,256,name="fire9")
-            drop = tf.layers.dropout(fire9,rate=0.5,training=train)
-        
-            conv10 = general_conv(drop,filters=200,kernel=1,stride=1,padding="SAME",name="conv10",relu=True,weight="Gaussian")
-        
-            avg_pool = tf.layers.average_pooling2d(conv10,pool_size=13,strides=1,name="pool_end")
-            pool_shape = tf.shape(avg_pool)
-            logits = tf.reshape(avg_pool,shape=(pool_shape[0],pool_shape[3]))
-                    
-            return logits
         
     #Model Definiton of SqueezeNet V1    
     def model_arc_v1(self,inputs,train,reuse=False):
@@ -112,88 +82,37 @@ class SqueezeNet:
             pool_shape = tf.shape(avg_pool)
             logits = tf.reshape(avg_pool,shape=(pool_shape[0],pool_shape[3]))
         
-            return logits
-        
-        
-    #Model Definition of SqueezeNet V0 Residual     
-    def model_arc_v0_res(self,inputs,train,reuse=False):
-        
-        with tf.variable_scope("squeezenet_v0_res",reuse=reuse):
-            conv1 = general_conv(inputs,filters=96,kernel=7,stride=2,padding="SAME",name="conv1",relu=True,weight="Xavier")
-            pool1 = tf.layers.max_pooling2d(conv1,pool_size=3,strides=2,name="pool1")
-            
-            fire2 = fire_module(pool1,16,64,64,name="fire2")
-            fire3 = fire_module(fire2,16,64,64,name="fire3")
-            
-            bypass_23 = tf.add(fire2,fire3,name="bypass_23")
-            
-            fire4 = fire_module(bypass_23,32,128,128,name="fire4")
-            pool2 = tf.layers.max_pooling2d(fire4,pool_size=3,strides=2,name="pool2")
-            fire5 = fire_module(pool2,32,128,128,name="fire5")
-            
-            bypass_45 = tf.add(pool2,fire5,name="bypass_45")
-            
-            fire6 = fire_module(bypass_45,48,192,192,name="fire6")
-            fire7 = fire_module(fire6,48,192,192,name="fire7")
-            fire8 = fire_module(fire7,64,256,256,name="fire8")
-        
-            pool3 = tf.layers.max_pooling2d(fire8,pool_size=3,strides=2,name="pool3")
-        
-            fire9 = fire_module(pool3,64,256,256,name="fire9")
-            
-            bypass_89 = tf.add(pool3,fire9,name="bypass_89")
-            
-            drop = tf.layers.dropout(bypass_89,rate=0.5,training=train)
-        
-            conv10 = general_conv(drop,filters=200,kernel=1,stride=1,padding="SAME",name="conv10",relu=True,weight="Gaussian")
-        
-            avg_pool = tf.layers.average_pooling2d(conv10,pool_size=13,strides=1,name="pool_end")
-        
-            pool_shape = tf.shape(avg_pool)
-            logits = tf.reshape(avg_pool,shape=(pool_shape[0],pool_shape[3]))
-        
-            return logits
-
+            return logits       
 
     #Function to calculate loss 
     def model_loss(self,inputs,label,train):
-        logits_v0 = self.model_arc_v0(inputs,train)
-        logits_v0_res = self.model_arc_v0_res(inputs,train)
         logits_v1 = self.model_arc_v1(inputs,train)
         
-        loss_v0 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits_v0,labels=label))
-        loss_v0_res = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits_v0_res,labels=label))
         loss_v1 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits_v1,labels=label))
         
-        return loss_v0,loss_v0_res,loss_v1
+        return loss_v1
     
     #Function to calculate prediction form models
     def model_prediction(self,inputs,train):
-        logits_v0 = self.model_arc_v0(inputs,train,True)
-        logits_v0_res = self.model_arc_v0_res(inputs,train,True)
         logits_v1 = self.model_arc_v1(inputs,train,True)
         
-        predict_v0 = tf.nn.softmax(logits_v0)
-        predict_v0_res = tf.nn.softmax(logits_v0_res)
         predict_v1 = tf.nn.softmax(logits_v1)
         
-        return predict_v0,predict_v0_res,predict_v1
+        return predict_v1
     
     #Function to optimize the models. 
-    def model_opti(self,loss_v0,loss_v0_res,loss_v1,lr_rate):
+    def model_opti(self,loss_v1,lr_rate):
         
         train_vars = tf.trainable_variables()
-        v0_vars = [var for var in train_vars if var.name.startswith('squeezenet_v0')]
-        v0_res_vars = [var for var in train_vars if var.name.startswith('squeezenet_v0_res')]
+
         v1_vars = [var for var in train_vars if var.name.startswith('squeezenet_v1')]        
         
         #Using Adam Optimizer 
         with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
-            v0_train_opt = tf.train.AdamOptimizer(lr_rate).minimize(loss_v0,var_list=v0_vars)
-            v0_res_train_opt = tf.train.AdamOptimizer(lr_rate).minimize(loss_v0_res,var_list=v0_res_vars)
+
             v1_train_opt = tf.train.AdamOptimizer(lr_rate).minimize(loss_v1,var_list=v1_vars)
             
-        return v0_train_opt, v0_res_train_opt, v1_train_opt
+        return v1_train_opt
         
         
         
--- test_squeezenet.py
+++ test_squeezenet.py
@@ -35,22 +35,17 @@ def test(sq_net,test_files,labels):
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         train_vars = tf.trainable_variables()
-        v0_vars = [var for var in train_vars if var.name.startswith('squeezenet_v0')]
-        v0_res_vars = [var for var in train_vars if var.name.startswith('squeezenet_v0_res')]
         v1_vars = [var for var in train_vars if var.name.startswith('squeezenet_v1')]
-        saver_v0  = tf.train.Saver(var_list=v0_vars,max_to_keep=None)
-        saver_v0_res  = tf.train.Saver(var_list=v0_res_vars,max_to_keep=None)
+
         saver_v1  = tf.train.Saver(var_list=v1_vars,max_to_keep=None)
         #change second argument of restore function to path where weights are saved (model wise)
-        saver_v0.restore(sess,model_dir+"v0_8000\\")
-        saver_v0_res.restore(sess,model_dir+"v0_res_8000\\")
+
         saver_v1.restore(sess,model_dir + "v1_8000\\")
         
         for i in range(len(test_files)):
             img = np.reshape(get_image_new(test_files[i],227,227),(1,227,227,3))
             pred_v0,pred_v0_res,pred_v1 = sess.run(sq_net.model_prediction(sq_net.inputs,False),feed_dict={sq_net.inputs:img})
-            pred_v0 = np.argmax(pred_v0,1)
-            pred_v0_res = np.argmax(pred_v0_res,1)
+
             pred_v1 = np.argmax(pred_v1,1)
             
             print ("img = %r pred_v0 = %r, %r pred_v0_res = %r, %r pred_v1 = %r, %r" %(i,pred_v0,labels[pred_v0],pred_v0_res,labels[pred_v0_res],pred_v1,labels[pred_v1]))
@@ -65,9 +60,9 @@ file_path = "squeeze\\dataset\\tiny-imagenet-200\\words.txt"
 #path of testing data 
 test_files = glob("squeeze\\dataset\\tiny-imagenet-200\\test\images\\*.JPEG")
 input_shape = 227,227,3
-batch_size = 256
+#batch_size = 256
 lr_rate = 0.04
-out_classes = 200
+out_classes = 2
 is_train = False
 tf.reset_default_graph()
 
